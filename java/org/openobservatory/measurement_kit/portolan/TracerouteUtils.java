7ef7229035af30f254f39c6c661191911c4cfb81
Simone Basso <bassosimone@gmail.com>
2016-09-06 17:50:51 +0200

    Add TracerouteUtils.java from Portolan

---
+++ java/org/openobservatory/measurement_kit/portolan/TracerouteUtils.java
@@ -0,0 +1,48 @@
+/*-
+ * Part of measurement-kit <https://measurement-kit.github.io/>.
+ * Measurement-kit is free software. See AUTHORS and LICENSE for more
+ * information on the copying conditions.
+ * =========================================================================
+ *
+ * Portions Copyright (c) 2015, Adriano Faggiani, Enrico Gregori,
+ * Luciano Lenzini, Valerio Luconi
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package java.org.openobservatory.measurement_kit.portolan;
+
+import java.util.regex.Pattern;
+
+public class TracerouteUtils {
+    public static boolean isIPv6(String address) {
+        boolean ret = Pattern.compile("^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$").matcher(address).matches();
+        if (ret) {
+            return ret;
+        }
+        return Pattern.compile("^((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)::((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)$").matcher(address).matches();
+    }
+
+    public static boolean isIPv4(String address) {
+        return Pattern.compile("^(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)(\\.(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)){3}$").matcher(address).matches();
+    }
+}
