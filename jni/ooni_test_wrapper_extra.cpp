12d4fa10c694256667fa85a0a48dbee09bd92b56
Simone Basso <bassosimone@gmail.com>
2016-10-13 16:52:11 +0200

    Exclude one case where we actually don't know what to do
    If we are not able to get the environment, we don't actually
    know what to do and hence better to throw and kill the app

--- jni/ooni_test_wrapper_extra.cpp
+++ jni/ooni_test_wrapper_extra.cpp
@@ -16,27 +16,25 @@ class Environment {
         int err = vm->GetEnv((void **)&env_, JNI_VERSION_1_6);
         if (err == JNI_EDETACHED) {
             if (vm->AttachCurrentThread(&env_, nullptr) != 0) {
-                env_ = nullptr; // Robustness
-                return;
+                throw std::runtime_error("cannot get environment");
             }
             is_attached_ = true;
         } else if (err != JNI_OK) {
-            env_ = nullptr; // Robustness
+            throw std::runtime_error("cannot get environment");
         } else {
-            /* nothing */
+            /* nothing */ ;
         }
     }
 
-    operator bool() { return env_ != nullptr; }
-
     JNIEnv *operator->() {
-        if (env_ == nullptr) {
+        if (env_ == nullptr) { // Check here only for robustness to refactoring
             throw std::runtime_error("null pointer exception");
         }
         return env_;
     }
 
     ~Environment() {
+        // Null check here only for robustness to refactoring
         if (is_attached_ && env_ != nullptr) {
             JavaVM *vm = mk_jni_get_saved_jvm(); // Throws on failure
             vm->DetachCurrentThread();
@@ -49,29 +47,17 @@ class Environment {
 };
 
 void OoniTestWrapper::on_log(jobject delegate) {
-    Environment environ;
-    if (!environ) {
-        return;
-    }
+    Environment environ; // Throws on error
     jobject global_cb = environ->NewGlobalRef(delegate); // Keep safe
     if (global_cb == nullptr) {
         return;
     }
     real_test_->logger->on_eof([global_cb]() {
-        Environment environ;
-        if (!environ) {
-            return;
-        }
+        Environment environ; // Throws on error
         environ->DeleteGlobalRef(global_cb);
     });
     real_test_->on_log([global_cb](uint32_t severity, const char *message) {
-        // Note: as stated above, we don't know in which thread we are running
-        // and whether it's attached to JVM, thus we follow the most robust and
-        // general approach implemented in the `Environment` class
-        Environment environ;
-        if (!environ) {
-            return;
-        }
+        Environment environ; // Throws on error
         jlong java_severity = severity;
         jstring java_message = environ->NewStringUTF(message);
         if (!java_message) {
@@ -95,20 +81,13 @@ void OoniTestWrapper::on_log(jobject delegate) {
 }
 
 void OoniTestWrapper::run(jobject callback) {
-    Environment environ;
-    if (!environ) {
-        return;
-    }
+    Environment environ; // Throws on error
     jobject global_cb = environ->NewGlobalRef(callback); // Keep safe
     if (global_cb == nullptr) {
         return;
     }
     real_test_->run([global_cb]() {
-        Environment environ;
-        if (!environ) {
-            // XXX leaking the reference
-            return;
-        }
+        Environment environ; // Throws on error
         jclass clazz = environ->GetObjectClass(global_cb);
         if (!clazz) {
             environ->DeleteGlobalRef(global_cb);
