dcb4a8961b03e5dc219423c060c523fcb5828090
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2016-10-23 11:06:18 -0400

    Added web connectivity test

---
+++ java/org/openobservatory/measurement_kit/nettests/WebConnectivityTest.java
@@ -0,0 +1,12 @@
+// Part of measurement-kit <https://measurement-kit.github.io/>.
+// Measurement-kit is free software. See AUTHORS and LICENSE for more
+// information on the copying conditions.
+
+package org.openobservatory.measurement_kit.nettests;
+
+public class WebConnectivityTest extends OoniTestBase {
+
+    public WebConnectivityTest() {
+        super("web_connectivity");
+    }
+}
