458f8fbc9de5e03502909d07424ab25a9ab62926
Simone Basso <bassosimone@gmail.com>
2018-08-02 20:46:18 +0200

    reduce diff

--- src/main/java/org/openobservatory/measurement_kit/nettests/EntryCallback.java
+++
@@ -1,8 +0,0 @@
-// Part of measurement-kit <https://measurement-kit.github.io/>.
-// Measurement-kit is free software. See AUTHORS and LICENSE for more
-// information on the copying conditions.
-
-package org.openobservatory.measurement_kit.nettests;
-
-public interface EntryCallback
-extends org.openobservatory.measurement_kit.swig.EntryCallback {}
