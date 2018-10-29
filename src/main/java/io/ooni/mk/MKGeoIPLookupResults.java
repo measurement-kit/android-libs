d8fc9920986fdfcd635e7cb755a734cf627d69af
Simone Basso <bassosimone@gmail.com>
2018-10-29 21:06:02 +0100

    Add JNI and Java bindings for mkgeoip.h

---
+++ src/main/java/io/ooni/mk/MKGeoIPLookupResults.java
@@ -0,0 +1,63 @@
+package io.ooni.mk;
+
+public class MKGeoIPLookupResults {
+  long handle = 0;
+
+  final static native boolean Good(long handle);
+
+  final static native double GetBytesSent(long handle);
+
+  final static native double GetBytesRecv(long handle);
+
+  final static native String GetProbeIP(long handle);
+
+  final static native long GetProbeASN(long handle);
+
+  final static native String GetProbeCC(long handle);
+
+  final static native String GetProbeOrg(long handle);
+
+  final static native byte[] GetLogs(long handle);
+
+  final static native void Delete(long handle);
+
+  protected MKGeoIPLookupResults(long n) {
+    handle = n;
+  }
+
+  public boolean good() {
+    return Good(handle);
+  }
+
+  public double getBytesSent() {
+    return GetBytesSent(handle);
+  }
+
+  public double getBytesRecv() {
+    return GetBytesRecv(handle);
+  }
+
+  public String getProbeIP() {
+    return GetProbeIP(handle);
+  }
+
+  public long getProbeASN() {
+    return GetProbeASN(handle);
+  }
+
+  public String getProbeCC() {
+    return GetProbeCC(handle);
+  }
+
+  public String getProbeOrg() {
+    return GetProbeOrg(handle);
+  }
+
+  public byte[] getLogs() {
+    return GetLogs(handle);
+  }
+
+  @Override public synchronized void finalize() {
+    Delete(handle);
+  }
+};
