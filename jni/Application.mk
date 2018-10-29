d5a8e0c5b47335d092b0d1ba5b10383b2f93ba1c
Simone Basso <bassosimone@gmail.com>
2018-10-29 20:16:51 +0100

    jni/Application.mk: bump to API 16
    The NDK does not support anymore APIs below that.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,5 @@
 APP_ABI := arm64-v8a armeabi-v7a x86 x86_64
-APP_PLATFORM := android-14
+APP_PLATFORM := android-16
 NDK_TOOLCHAIN_VERSION := clang
 APP_STL := c++_static
 APP_CPPFLAGS += -fexceptions -frtti
