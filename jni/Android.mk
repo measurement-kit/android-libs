40b8e90afa68bc8bf0e3bf6838994311257c661d
Simone Basso <bassosimone@gmail.com>
2018-10-29 21:07:22 +0100

    Make sure we use build 9 of everything

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,35 +2,45 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libGeoIP
-LOCAL_SRC_FILES := ../MK_DIST/android/geoip-api-c/1.6.12-4/$(TARGET_ARCH_ABI)/lib/libGeoIP.a
+LOCAL_SRC_FILES := ../MK_DIST/android/geoip-api-c/1.6.12-9/$(TARGET_ARCH_ABI)/lib/libGeoIP.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libcrypto
-LOCAL_SRC_FILES := ../MK_DIST/android/libressl/2.7.4-1/$(TARGET_ARCH_ABI)/lib/libcrypto.a
+LOCAL_SRC_FILES := ../MK_DIST/android/libressl/2.7.4-9/$(TARGET_ARCH_ABI)/lib/libcrypto.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libevent
-LOCAL_SRC_FILES := ../MK_DIST/android/libevent/2.1.8-7/$(TARGET_ARCH_ABI)/lib/libevent.a
+LOCAL_SRC_FILES := ../MK_DIST/android/libevent/2.1.8-9/$(TARGET_ARCH_ABI)/lib/libevent.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libevent_openssl
-LOCAL_SRC_FILES := ../MK_DIST/android/libevent/2.1.8-7/$(TARGET_ARCH_ABI)/lib/libevent_openssl.a
+LOCAL_SRC_FILES := ../MK_DIST/android/libevent/2.1.8-9/$(TARGET_ARCH_ABI)/lib/libevent_openssl.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libevent_pthreads
-LOCAL_SRC_FILES := ../MK_DIST/android/libevent/2.1.8-7/$(TARGET_ARCH_ABI)/lib/libevent_pthreads.a
+LOCAL_SRC_FILES := ../MK_DIST/android/libevent/2.1.8-9/$(TARGET_ARCH_ABI)/lib/libevent_pthreads.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libssl
-LOCAL_SRC_FILES := ../MK_DIST/android/libressl/2.7.4-1/$(TARGET_ARCH_ABI)/lib/libssl.a
+LOCAL_SRC_FILES := ../MK_DIST/android/libressl/2.7.4-9/$(TARGET_ARCH_ABI)/lib/libssl.a
 include $(PREBUILT_STATIC_LIBRARY)
 
-MK_DIR=../MK_DIST/android/measurement-kit/0.9.0-alpha.9-1/$(TARGET_ARCH_ABI)/
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcurl
+LOCAL_SRC_FILES := ../MK_DIST/android/curl/7.61.1-9/$(TARGET_ARCH_ABI)/lib/libcurl.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmaxminddb
+LOCAL_SRC_FILES := ../MK_DIST/android/libmaxminddb/1.3.2-9/$(TARGET_ARCH_ABI)/lib/libmaxminddb.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+MK_DIR=../MK_DIST/android/measurement-kit/0.9.0-alpha.11-9/$(TARGET_ARCH_ABI)/
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmeasurement_kit_static # another name to avoid conflict
@@ -40,8 +50,8 @@ include $(PREBUILT_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_LDLIBS := -llog -latomic
 # Note to self: the order of libraries matters
-LOCAL_STATIC_LIBRARIES := measurement_kit_static GeoIP event_openssl ssl       \
-                          crypto event event_pthreads
+LOCAL_STATIC_LIBRARIES := measurement_kit_static libmaxminddb libcurl          \
+                          GeoIP event_openssl ssl crypto event event_pthreads
 LOCAL_MODULE := measurement_kit
 LOCAL_CPPFLAGS += -I jni/$(MK_DIR)/include                                     \
                   -std=c++14                                                   \
