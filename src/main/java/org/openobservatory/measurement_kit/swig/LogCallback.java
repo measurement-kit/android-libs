ec8d126c64c32cbb4405b1bdcd0e6427474e8fdf
Simone Basso <bassosimone@gmail.com>
2018-11-06 18:28:41 +0100

    Remove all legacy APIs

--- src/main/java/org/openobservatory/measurement_kit/swig/LogCallback.java
+++
@@ -1,9 +0,0 @@
-// Part of measurement-kit <https://measurement-kit.github.io/>.
-// Measurement-kit is free software. See AUTHORS and LICENSE for more
-// information on the copying conditions.
-
-package org.openobservatory.measurement_kit.swig;
-
-public interface LogCallback {
-    public void callback(final long verbosity, final String message);
-}
