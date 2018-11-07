bb1f7f13156a15b92d75f8757eeb013522e06b93
Simone Basso <bassosimone@gmail.com>
2018-11-07 10:18:30 +0100

    Delete most sources from this repository
    We will copy them from the mkall-java submodule. Eventually the
    submodule will be cool enough that this step is not needed.

--- src/main/java/io/ooni/mk/MKVersion.java
+++
@@ -1,9 +0,0 @@
-// Part of measurement-kit <https://measurement-kit.github.io/>.
-// Measurement-kit is free software. See AUTHORS and LICENSE for more
-// information on the copying conditions.
-
-package io.ooni.mk;
-
-public class MKVersion {
-    public final static native String getVersion();
-}
