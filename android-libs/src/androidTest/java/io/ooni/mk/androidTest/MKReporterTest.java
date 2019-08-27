f81567e45e8ba2bf78c8e44ad0318ffb459ce783
Simone Basso <bassosimone@gmail.com>
2019-08-27 18:53:36 +0200

    MKReportTest.java: start testing multiple submissions

--- android-libs/src/androidTest/java/io/ooni/mk/androidTest/MKReporterTest.java
+++ android-libs/src/androidTest/java/io/ooni/mk/androidTest/MKReporterTest.java
@@ -20,20 +20,39 @@ import io.ooni.mk.MKResourcesManager;
         System.loadLibrary("measurement_kit");
     }
 
-    static private String measurement = "{\"data_format_version\":\"0.2.0\",\"id\":\"9858087b-15b9-4913-a87e-2520f85393b2\",\"input\":null,\"input_hashes\":[],\"measurement_start_time\":\"2019-07-26 10:39:09\",\"options\":[],\"probe_asn\":\"AS0\",\"probe_cc\":\"ZZ\",\"probe_city\":null,\"probe_ip\":\"127.0.0.1\",\"report_id\":\"20190726T103909Z_AS0_KTgS3f6IgfdSuCoqQE1A31u4AtjpbO6Ywj0LPkn7c4mTd1lBut\",\"software_name\":\"measurement_kit\",\"software_version\":\"0.10.4\",\"test_helpers\":{\"backend\":\"http://37.218.247.95:80\"},\"test_keys\":{},\"test_name\":\"dummy\",\"test_runtime\":0.07317614555358887,\"test_start_time\":\"2019-07-26 10:39:05\",\"test_version\":\"0.0.1\"}";
+    static private String baseMeasurement = "{\"data_format_version\":\"0.2.0\",\"id\":\"9858087b-15b9-4913-a87e-2520f85393b2\",\"input\":null,\"input_hashes\":[],\"measurement_start_time\":\"2019-07-26 10:39:09\",\"options\":[],\"probe_asn\":\"AS0\",\"probe_cc\":\"ZZ\",\"probe_city\":null,\"probe_ip\":\"127.0.0.1\",\"report_id\":\"20190726T103909Z_AS0_KTgS3f6IgfdSuCoqQE1A31u4AtjpbO6Ywj0LPkn7c4mTd1lBut\",\"software_name\":\"measurement_kit\",\"software_version\":\"0.10.4\",\"test_helpers\":{\"backend\":\"http://37.218.247.95:80\"},\"test_keys\":{},\"test_name\":\"dummy\",\"test_runtime\":0.07317614555358887,\"test_start_time\":\"2019-07-26 10:39:05\",\"test_version\":\"0.0.1\"}";
 
-    @Test public void perform() {
+    private MKReporterTask initialize() {
         Context context = ApplicationProvider.getApplicationContext();
         boolean okay = MKResourcesManager.maybeUpdateResources(context);
         Truth.assertThat(okay).isTrue();
-        MKReporterTask task = new MKReporterTask(
-                "android-libs", "0.1.0",
+        return new MKReporterTask("android-libs", "0.1.0",
                 MKResourcesManager.getCABundlePath(context));
-        MKReporterResults results = task.submit(measurement, 300);
+    }
+
+    private MKReporterResults submit(MKReporterTask task, String measurement) {
+        long timeout = 300;
+        MKReporterResults results = task.submit(measurement, timeout);
         System.out.print(results.getLogs());
         System.out.println("updatedMeasurement  : " + results.getUpdatedSerializedMeasurement());
         System.out.println("updatedReportID     : " + results.getUpdatedReportID());
         System.out.println("serializedStats     : " + results.getSerializedStats());
         Truth.assertThat(results.isGood()).isTrue();
+        return results;
+    }
+
+    @Test public void submitOne() {
+        submit(initialize(), baseMeasurement);
+    }
+
+    @Test public void submitMany() {
+        MKReporterTask task = initialize();
+        String m = baseMeasurement.replace("\"input\":null", "\"input\":\"a\"");
+        submit(task, m);
+        m = baseMeasurement.replace("\"input\":null", "\"input\":\"b\"");
+        submit(task, m);
+        m = baseMeasurement.replace("\"test_name\":\"dummy\"",
+                                    "\"test_name\":\"x\"");
+        submit(task, m);
     }
 }
